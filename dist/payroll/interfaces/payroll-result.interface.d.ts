a1ab2e59b216056800ac5c0a6cd0670a21779c8a
goziyyy <fariscraft709@gmail.com>
2024-12-28 19:07:51 +0700

    remove dist

--- dist/payroll/interfaces/payroll-result.interface.d.ts
+++
@@ -1,7 +0,0 @@
-export interface PayrollResult {
-    totalGaji: number;
-    gajiLembur: number;
-    gajiPokok: number;
-    jamNormal: number;
-    jamLembur: number;
-}
--- dist/payroll/payroll.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { CalculatePayrollDto } from './dto/calculate-payroll.dto';
-import { PayrollService } from './payroll.service';
-export declare class PayrollController {
-    private readonly payrollService;
-    constructor(payrollService: PayrollService);
-    calculate(calculatePayrollDto: CalculatePayrollDto): import("./interfaces/payroll-result.interface").PayrollResult;
-}
